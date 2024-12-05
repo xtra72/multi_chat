7e70180813f5d8fe2b57c56f5752cab670aa3461
Xtra <xtra72@gmail.com>
2024-12-05 14:22:53 +0900

    .

--- src/main/java/com/nhnacademy/Server.java
+++ src/main/java/com/nhnacademy/Server.java
@@ -6,10 +6,13 @@ import java.io.PrintStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Queue;
+import java.util.function.Consumer;
 
 class Handler extends Thread {
     static List<Handler> handlerList = new LinkedList<>();
@@ -31,18 +34,36 @@ class Handler extends Thread {
         return clientId;
     }
 
+    public void sendMessage(String from, String[] messages) {
+        socketOut.println(String.format("@ 1 message %s", from));
+        for (String message : messages) {
+            socketOut.println(message);
+        }
+    }
+
     @Override
     public void run() {
         handlerList.add(this);
         try {
+            Queue<String> tempQueue = new LinkedList<>();
             socketOut = new PrintStream(socket.getOutputStream());
 
             Thread receiver = new Thread(() -> {
                 try {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                    String line = reader.readLine();
-                    receiveQueue.add(line);
+                    while (!Thread.currentThread().isInterrupted()) {
+                        String line = reader.readLine();
+                        if (line.length() != 0) {
+                            tempQueue.add(line);
+                        } else {
+                            synchronized (receiveQueue) {
+                                while (!tempQueue.isEmpty()) {
+                                    receiveQueue.add(tempQueue.poll());
+                                }
+                            }
+                        }
+                    }
 
                 } catch (IOException ignore) {
                 }
@@ -51,35 +72,69 @@ class Handler extends Thread {
             receiver.start();
 
             while (!Thread.currentThread().isInterrupted()) {
-                if (!receiveQueue.isEmpty()) {
-                    String line = receiveQueue.peek();
+                synchronized (receiveQueue) {
+                    if (!receiveQueue.isEmpty()) {
+                        String line = receiveQueue.peek();
 
-                    if (!line.isEmpty() && (line.charAt(0) == COMMAND_REQUEST)) {
-                        line = receiveQueue.poll();
+                        if (!line.isEmpty() && (line.charAt(0) == COMMAND_REQUEST)) {
+                            line = receiveQueue.poll();
+                            System.out.println("Received : " + line);
+                            String[] fields = line.split(" ");
+                            if (fields.length != 4) {
+                                System.out.println("Invalid command");
+                            } else {
+                                if (fields[2].equals("connect")) {
+                                    try {
+                                        getHandler(fields[3]);
+                                        // 중복 등록!!
+                                        response(fields[1], "deny", fields[3], null);
+                                        break;
+                                    } catch (NoSuchElementException ignore) {
+                                        clientId = fields[3];
+                                        response(fields[1], "ok", fields[3], null);
+                                    }
+                                } else if (fields[2].equals("message")) {
+                                    System.out.println("Command : message - " + receiveQueue.size());
+                                    // # <mid> message <target_client_id>
+                                    // hello, world!
+                                    //
+                                    try {
+                                        Handler target = getHandler(fields[3]);
+                                        // 대상 클라이언트가 존재합니다!
+                                        // try {
+                                        // System.out.println("Command : message - 타겟을 찾았습니다.");
+                                        // // Thread.sleep(5000);
+                                        // } catch (InterruptedException e) {
+                                        // // TODO Auto-generated catch block
+                                        // e.printStackTrace();
+                                        // }
+                                        System.out.println("Command : message - " + receiveQueue.size());
+                                        List<String> messages = new LinkedList<>();
+                                        while (!receiveQueue.isEmpty()) {
+                                            String message = receiveQueue.poll();
 
-                        String[] fields = line.split(" ");
-                        if (fields.length != 4) {
-                            System.out.println("Invalid command");
-                        } else {
-                            if (fields[2].equals("connect")) {
-                                try {
-                                    getHandler(fields[3]);
-                                    // 중복 등록!!
-                                    response(fields[1], "deny", fields[3], null);
-                                    break;
-                                } catch (NoSuchElementException ignore) {
-                                    clientId = fields[3];
-                                    response(fields[1], "ok", fields[3], null);
+                                            if (!message.isEmpty()) {
+                                                messages.add(message);
+                                            } else {
+                                                break;
+                                            }
+                                        }
+
+                                        target.sendMessage(getClientId(), messages.toArray(new String[0]));
+
+                                    } catch (NoSuchElementException ignore) {
+                                        System.out.println("Command : message - 타겟을 찾을 수 없습니다");
+                                    }
                                 }
                             }
-                        }
 
-                    }
-                } else {
-                    try {
-                        Thread.sleep(interval);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
+                        }
+                    } else {
+                        try {
+                            Thread.sleep(interval);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
                 }
             }
@@ -145,23 +200,29 @@ class Handler extends Thread {
 
 public class Server extends Thread {
     int port;
+    Map<String, Consumer<String[]>> commandSet = new HashMap<>();
 
     public Server(int port) {
         this.port = port;
+
+        commandSet.put("show", args -> {
+            if (args.length > 1) {
+                if (args[0].equals("client")) {
+                    for (int i = 0; i < Handler.getCount(); i++) {
+                        System.out.println(String.format("%d : %s",
+                                i + 1, Handler.getHandler(i).getClientId()));
+                    }
+                }
+            }
+        });
     }
 
     @Override
     public void run() {
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            while (true) {
-                Socket socket = serverSocket.accept();
-
-                Handler handler = new Handler(socket);
-
-                handler.start();
-
+        Thread console = new Thread(() -> {
+            try {
                 BufferedReader terminalIn = new BufferedReader(new InputStreamReader(System.in));
-                while (true) {
+                while (!Thread.currentThread().isInterrupted()) {
                     String line = terminalIn.readLine();
                     String[] fields = line.split("\\s");
 
@@ -178,9 +239,28 @@ public class Server extends Thread {
                         }
                     }
                 }
+            } catch (IOException ignore) {
+            }
+
+        });
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            console.start();
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                Handler handler = new Handler(socket);
+
+                handler.start();
+
             }
         } catch (IOException ignore) {
+        } finally {
+            console.interrupt();
         }
+
     }
 
     public static void main(String[] args) {
